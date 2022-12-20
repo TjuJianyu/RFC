868ecad66ce5665d78353e3f7ab94929791f56fc
Jianyu Zhang <jianyu@nyu.edu>
2022-12-20 10:17:28 -0500

    Update camelyon17_rfc_r1.sh

--- scripts/camelyon17/camelyon17_rfc_r1.sh
+++ scripts/camelyon17/camelyon17_rfc_r1.sh
@@ -35,10 +35,10 @@ final_mode=${eval_mode[$SLURM_ARRAY_TASK_ID]}
 final_seed=${seeds[$SLURM_ARRAY_TASK_ID]}
 final_wd=${wd[$SLURM_ARRAY_TASK_ID]}
 
-python examples/analysis_earlystop.py --result_dir results/erm_camelyon17_seed${final_seed}/
+python src/analysis_earlystop.py --result_dir results/erm_camelyon17_seed${final_seed}/
 
 
-python examples/run_expt_rfc_eval.py --version "1.0" --root_dir data/camelyon17/ \
+python src/run_expt_rfc_eval.py --version "1.0" --root_dir data/camelyon17/ \
 --log_dir results/erm_camelyon17_seed${final_seed} --eval_only \
 --eval_epoch `cat results/erm_camelyon17_seed${final_seed}/eval_epochs_${final_mode}.txt` \
 --dataset camelyon17 --algorithm ERM --model densenet121 --seed ${final_seed}
@@ -49,13 +49,13 @@ resdir=results/rfc_camelyon17_eval${final_mode}_wd${final_wd}_seed${final_seed}/
 mkdir $resdir
 
 
-python examples/generate_groups.py --dataset camelyon17 \
+python src/generate_groups.py --dataset camelyon17 \
 --pred_dirs results/erm_camelyon17_seed${final_seed}/ \
 --pred_epochs `cat results/erm_camelyon17_seed${final_seed}/eval_epochs_${final_mode}.txt` \
 --pred_seed ${final_seed}  --result_dir ${resdir}  --result_dir ${resdir}
 
 
-python examples/run_expt_rfc.py --version "1.0" --root_dir data/camelyon17/ --save_step 1 \
+python src/run_expt_rfc.py --version "1.0" --root_dir data/camelyon17/ --save_step 1 \
 --rfc_groups_dir ${resdir} --log_dir ${resdir} --weight_decay ${final_wd} \
 --dataset camelyon17 --algorithm groupDRO --model densenet121 --seed ${final_seed}
 
