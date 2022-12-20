0f72189555fa9e7149980d561a634fd06486ed9a
Jianyu Zhang <jianyu@nyu.edu>
2022-12-20 10:16:51 -0500

    Update camelyon17_rfc_r0r1r2_syn_vrex_linear.sh

--- scripts/camelyon17/camelyon17_rfc_r0r1r2_syn_vrex_linear.sh
+++ scripts/camelyon17/camelyon17_rfc_r0r1r2_syn_vrex_linear.sh
@@ -44,10 +44,10 @@ resdir=results/vrexlinear_camelyon17_${mark}_lambda${final_lambda}_seed${final_s
 syndir=results/syn_camelyon17_3round_r0r1r2_even_evaliid_wd${final_synwd}_seed${final_seed}
 mkdir $resdir
 
-python examples/analysis_earlystop.py --result_dir ${syndir}
+python src/analysis_earlystop.py --result_dir ${syndir}
 
 
-python examples/run_expt_synthesis_convert_model.py  \
+python src/run_expt_synthesis_convert_model.py  \
 --version "1.0" --root_dir data/camelyon17/ --save_step 1  \
 --rfc_groups_dir  ${syndir} \
 --log_dir ${syndir} \
@@ -58,7 +58,7 @@ python examples/run_expt_synthesis_convert_model.py  \
 
 echo "converted model"
 
-python examples/run_expt.py --version "1.0" --root_dir data/camelyon17/ \
+python src/run_expt.py --version "1.0" --root_dir data/camelyon17/ \
 --log_dir ${resdir} \
 --dataset camelyon17 --algorithm VREX --model densenet121 --seed ${final_seed} \
 --save_step 1 --irm_penalty_anneal_iters 0 --irm_lambda ${final_lambda} --resume --freeze_featurizer True 
